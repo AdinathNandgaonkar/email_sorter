380a9f3692af81217d115c8293baaeb29d408711
Adinath Nandgaonkar <adinathnandgaonkar@gmail.com>
2022-07-12 18:56:51 +0200

    daily works

--- email_sorter.java
+++ email_sorter.java
@@ -1,11 +1,12 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class email_sorter implements Serializable{  
-        static int dailyEmailSorted;
-        static int dailyEmailRecruitment;
-        static int dailyEmailSpam;
-        static int dailyEmailSales;
-        static int dailyEmailReception;
+        // static int dailyEmailSorted;
+        // static int dailyEmailRecruitment;
+        // static int dailyEmailSpam;
+        // static int dailyEmailSales;
+        // static int dailyEmailReception;
         static String emailCV = "this is an example text containing cv";
         static String emailPromo = "this is an example text containing promo";
         static String emailAdvertising = "this is an example text containing advertising";
@@ -19,7 +20,7 @@ public class email_sorter implements Serializable{
         static int totalEmailSpam = 0;
         static int totalEmailSales = 0;
         static int totalEmailReception = 0;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         // Email sampleEmail = new Email();
         // sampleEmail.sender = "Michael";
@@ -62,18 +63,57 @@ public class email_sorter implements Serializable{
                 logicalSize--;
             }
             totalEmailSorted++;
-            dailyEmailSorted++;
         }
 
+
+        FileReader fileReader = new FileReader("DailyEmailsCount.dat");
+        BufferedReader reader = new BufferedReader(fileReader);
+        String dailyEmailCount = reader.readLine();
+        fileReader.close();
+        
+        String memory = dailyEmailCount;
+        int[] oldElements = Arrays.stream(memory.split(":")).mapToInt(Integer::parseInt).toArray ();
+        int[] newElements = Arrays.stream(memory.split(":")).mapToInt(Integer::parseInt).toArray ();
+        String updatedElements = "";
+        
+        newElements[0] = totalEmailSorted;
+        newElements[1] = totalEmailRecruitment;
+        newElements[2] = totalEmailSpam;
+        newElements[3] = totalEmailSales;
+        newElements[4] = totalEmailReception;
+        
+        int[] dailyTotal = new int[5];
+        dailyTotal[0] = oldElements[0] + newElements[0];
+        dailyTotal[1] = oldElements[1] + newElements[1];
+        dailyTotal[2] = oldElements[2] + newElements[2];
+        dailyTotal[3] = oldElements[3] + newElements[3];
+        dailyTotal[4] = oldElements[4] + newElements[4];
+        
+        StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < dailyTotal.length; i++) {
+            stringBuilder.append(dailyTotal[i]);
+                if (i == dailyTotal.length - 1){
+                } else {
+                stringBuilder.append(":");
+                }
+            }
+        updatedElements = stringBuilder.toString();
+
+        FileWriter fileWriter = new FileWriter("DailyEmailsCount.dat");
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        writer.write(updatedElements);
+        writer.close();
+        
+        
         System.out.println("total emails sorted " + totalEmailSorted);
         System.out.println("total emails recruitment " + totalEmailRecruitment);
         System.out.println("total emails spam " + totalEmailSpam);
         System.out.println("total emails sales " + totalEmailSales);
         System.out.println("total emails reception " + totalEmailReception);
-        System.out.println("daily emails sorted " + dailyEmailSorted);
-        System.out.println("daily emails recruitment " + dailyEmailRecruitment);
-        System.out.println("daily emails spam " + dailyEmailSpam);
-        System.out.println("daily emails sales " + dailyEmailSales);
-        System.out.println("daily emails reception " + dailyEmailReception);
+        System.out.println("daily emails sorted " + dailyTotal[0]);
+        System.out.println("daily emails recruitment " + dailyTotal[1]);
+        System.out.println("daily emails spam " + dailyTotal[2]);
+        System.out.println("daily emails sales " + dailyTotal[3]);
+        System.out.println("daily emails reception " + dailyTotal[4]);
     }
 }
